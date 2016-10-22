4077db8db7234b5a65cc7b32047b091a8bdb4f81
ArmenHagopian <14040@ecam.be>
2016-10-22 22:14:02 +0200

    One more method for Teacher

--- Teacher.cs
+++ Teacher.cs
@@ -6,20 +6,34 @@ namespace Relations_classes_objets
 	public class Teacher : Person
 	{
 		private int _salary;
+		private string _trigram;
 		private List<Student> Students = new List<Student>();
-		public Teacher(string Firstname, string Lastname, int Salary) : base(Firstname, Lastname)
+		public Teacher(string Firstname, string Lastname, int Salary, string Trigram) : base(Firstname, Lastname)
 		{
 			this._salary = Salary;
+			this._trigram = Trigram;
 		}
 
 		public void AddStudent(Student student)
 		{
 			Students.Add(student);
 		}
-
+		public string DisplayStudents()
+		{
+			string display = string.Format("Ci-desssous sont repris tous les eleves de {0} {1} :\n", Firstname, Lastname);
+			foreach (Student student in Students)
+			{
+				display += student + "\n";
+			}
+			return display;
+		}
 		public string Salary
 		{
 			get { return Convert.ToString(this._salary); }
+		}
+		public string Trigram
+		{
+			get { return this._trigram;}
 		}
 	}
 }
