1600c4a5f55a312462b8d7998e0ff2ba7f2982ba
ArmenHagopian <14040@ecam.be>
2016-10-23 12:13:29 +0200

    New file for display cases, while scope till the user doesn't give right answer, new quit functionality

---
+++ DisplayCase.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EvaluationSystem
+{
+	public class DisplayCase
+	{
+		private string _input;
+		List<Student> _studentslist;
+		List<Teacher> _teacherslist;
+		List<Activity> _activitieslist;
+		public DisplayCase(string input, List<Student> studentslist, List<Teacher> teacherslist, List<Activity> activitieslist)
+		{
+			this._input = input;
+			this._studentslist = studentslist;
+			this._teacherslist = teacherslist;
+			this._activitieslist = activitieslist;
+		}
+
+		public override string ToString()
+		{
+			switch (this._input)
+			{
+				//case "qui":
+				//	return "stop";
+				case "1":
+
+					string studentdisplay = "\nVoici la liste des etudiants\n";
+					string line;
+					if (this._studentslist.Count == 0)
+					{
+						// Read the file line by line.
+						System.IO.StreamReader allStudents = new System.IO.StreamReader("Students.txt");
+						while ((line = allStudents.ReadLine()) != null)
+						{
+							string[] splitstudents = line.Split(new Char[] { ';' });
+							Student student = new Student(splitstudents[0], splitstudents[1], Convert.ToInt32(splitstudents[2]));
+							this._studentslist.Add(student);
+						}
+					}
+					int counter = 1;
+					foreach (Student eachstudent in this._studentslist)
+					{
+						studentdisplay += string.Format("{0}/ {1}, Matricule : {2}\n", counter, eachstudent.DisplayName(), eachstudent.Matricule);
+						counter++;
+					}
+					
+					return studentdisplay + "\nChoisir un etudiant en donnant le numero de son matricule ou taper B pour revenir au menu principal.";
+
+				case "2":
+
+					string teacherdisplay = "\nVoici la liste des enseignants\n";
+					if (this._teacherslist.Count == 0)
+					{
+						System.IO.StreamReader allTeachers = new System.IO.StreamReader("Teachers.txt");
+						while ((line = allTeachers.ReadLine()) != null)
+						{
+							string[] splitteachers = line.Split(new Char[] { ';' });
+							Teacher teacher = new Teacher(splitteachers[0], splitteachers[1], Convert.ToInt32(splitteachers[2]), splitteachers[3]);
+							this._teacherslist.Add(teacher);
+						}
+					}
+						counter = 1;
+						foreach (Teacher eachteacher in this._teacherslist)
+						{
+							teacherdisplay += string.Format("{0}/ {1}, Trigramme : {2}\n", counter, eachteacher.DisplayName(), eachteacher.Trigram);
+							counter++;
+						}
+					
+					return teacherdisplay + "\nChoisir un enseignant en donnant son trigramme ou taper B pour revenir au menu principal.";
+					
+				case "3":
+
+					Console.WriteLine("\nVoici la liste des activites\n");
+					if (this._teacherslist.Count == 0)
+					{
+						System.IO.StreamReader allTeachers = new System.IO.StreamReader("Teachers.txt");
+						while ((line = allTeachers.ReadLine()) != null)
+						{
+							string[] splitteachers = line.Split(new Char[] { ';' });
+							Teacher teacher = new Teacher(splitteachers[0], splitteachers[1], Convert.ToInt32(splitteachers[2]), splitteachers[3]);
+							this._teacherslist.Add(teacher);
+						}
+					}
+
+					if (this._activitieslist.Count == 0)
+					{
+						System.IO.StreamReader allactivities = new System.IO.StreamReader("Activities.txt");
+
+						while ((line = allactivities.ReadLine()) != null)
+						{
+							string[] splitactivities = line.Split(new Char[] { ';' });
+							foreach (Teacher element in this._teacherslist)
+							{
+								if (element.Trigram == splitactivities[3])
+								{
+									Teacher teacherobject = new Teacher(element.Firstname, element.Lastname,
+																		Convert.ToInt32(element.Salary), element.Trigram);
+									Activity activity = new Activity(Convert.ToInt32(splitactivities[0]), splitactivities[1],
+																	 splitactivities[2], teacherobject);
+									this._activitieslist.Add(activity);
+								}
+							}
+						}
+					}
+					string activitydisplay = "Nom du cours" + "\tCode du cours" + "\tNbre ECTS" + "\tEnseignant\n";
+					foreach (Activity eachactivity in this._activitieslist)
+					{
+						//more or less tab depending on the length of the name of the activity 
+						if (eachactivity.Name.Length < 10)
+						{
+							activitydisplay += eachactivity.Name + "\t\t" + eachactivity.Code + "\t\t" + eachactivity.ECTS + "\t\t" + eachactivity.Teacher.Trigram + "\n";
+						}
+						else
+						{
+							activitydisplay += eachactivity.Name + "\t" + eachactivity.Code + "\t\t" + eachactivity.ECTS + "\t\t" + eachactivity.Teacher.Trigram + "\n";
+						}
+					}
+
+					return activitydisplay + "\nChoisir une activite en donnant son code ou taper B pour revenir au menu principal.";
+
+				default:
+
+					return "Veuillez entrer l'un des numeros de liste existant";
+				
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -18,91 +18,46 @@ namespace EvaluationSystem
 			List<Student> studentslist = new List<Student>();
 			List<Teacher> teacherslist = new List<Teacher>();
 			List<Activity> activitieslist = new List<Activity>();
+			string input = "";
+			string a = "";
+			//string stop = "";
 
-			string a = Console.ReadLine();
-			if (a == "1")
+			input = Console.ReadLine();
+			while (input != "quit" & a!="quit")
 			{
-				Console.WriteLine("\nVoici la liste des etudiants\n");
-				int counter = 0;
-				string line;
-				// Read the file line by line.
-				System.IO.StreamReader allStudents = new System.IO.StreamReader("Students.txt");
-				while ((line = allStudents.ReadLine()) != null)
+				DisplayCase display = new DisplayCase(input, studentslist, teacherslist, activitieslist);
+				//stop = display.ToString();
+				Console.WriteLine(display);
+				while (display.ToString() == "Veuillez entrer l'un des numeros de liste existant")
 				{
-					string[] splitstudents = line.Split(new Char[] { ';' });
-					Student student = new Student(splitstudents[0], splitstudents[1], Convert.ToInt32(splitstudents[2]));
-					studentslist.Add(student);
-					counter++;
+					input = Console.ReadLine();
+					display = new DisplayCase(input, studentslist, teacherslist, activitieslist);
+					Console.WriteLine(display);
 				}
-				foreach (Student eachstudent in studentslist)
+				a = Console.ReadLine();
+				while (a == "B" & (a != "quit" & input !="quit"))
 				{
-					Console.WriteLine(string.Format("{0}, Matricule : {1}", eachstudent.DisplayName(), eachstudent.Matricule));
-				}
-			}
-			else if (a == "2")
-			{
-				Console.WriteLine("\nVoici la liste des enseignants\n");
-				string line;
-				System.IO.StreamReader allTeachers = new System.IO.StreamReader("Teachers.txt");
-				while ((line = allTeachers.ReadLine()) != null)
-				{
-					string[] splitteachers = line.Split(new Char[] { ';' });
-					Teacher teacher = new Teacher(splitteachers[0], splitteachers[1], Convert.ToInt32(splitteachers[2]), splitteachers[3]);
-					teacherslist.Add(teacher);
-				}
-				foreach (Teacher eachteacher in teacherslist)
-				{
-					Console.WriteLine(string.Format("{0}, Trigramme : {1}", eachteacher.DisplayName(), eachteacher.Trigram));
-				}
-			}
-			else if (a == "3")
-			{
-				Console.WriteLine("\nVoici la liste des activites\n");
-				string line;
-				System.IO.StreamReader allTeachers = new System.IO.StreamReader("Teachers.txt");
-				while ((line = allTeachers.ReadLine()) != null)
-				{
-					string[] splitteachers = line.Split(new Char[] { ';' });
-					Teacher teacher = new Teacher(splitteachers[0], splitteachers[1], Convert.ToInt32(splitteachers[2]), splitteachers[3]);
-					teacherslist.Add(teacher);
-				}
-
-				System.IO.StreamReader allactivities = new System.IO.StreamReader("Activities.txt");
-
-				while ((line = allactivities.ReadLine()) != null)
-				{
-					string[] splitactivities = line.Split(new Char[] { ';' });
-					foreach (Teacher element in teacherslist)
+					Console.WriteLine("=== Bienvenue dans le meilleur logiciel de gestion d'Evaluations ===");
+					Console.WriteLine("1/ Liste des étudiants.");
+					Console.WriteLine("2/ Liste des enseignants.");
+					Console.WriteLine("3/ Liste des activités.");
+					Console.WriteLine("Veuillez entrer le numéro de la liste que vous souhaitez consulter");
+					input = Console.ReadLine();
+					while (input != "quit" & a != "quit")
 					{
-						if (element.Trigram == splitactivities[3])
+						display = new DisplayCase(input, studentslist, teacherslist, activitieslist);
+						Console.WriteLine(display);
+						while (display.ToString() == "Veuillez entrer l'un des numeros de liste existants" & (input != "quit" & a != "quit"))
 						{
-							Teacher teacherobject = new Teacher(element.Firstname, element.Lastname,
-							                                    Convert.ToInt32(element.Salary), element.Trigram);
-							Activity activity = new Activity(Convert.ToInt32(splitactivities[0]), splitactivities[1],
-															 splitactivities[2], teacherobject);
-							activitieslist.Add(activity);
+							input = Console.ReadLine();
+							display = new DisplayCase(input, studentslist, teacherslist, activitieslist);
+							Console.WriteLine(display);
 						}
+						a = Console.ReadLine();
 					}
 				}
-				string activitydisplay = "Nom du cours" + "\tCode du cours" + "\tNbre ECTS" + "\tEnseignant\n";
-				foreach (Activity eachactivity in activitieslist)
-				{
-					//more or less tab depending on the length of the name of the activity 
-					if (eachactivity.Name.Length < 10)
-					{
-						activitydisplay += eachactivity.Name + "\t\t" + eachactivity.Code + "\t\t" + eachactivity.ECTS + "\t\t" + eachactivity.Teacher.Trigram + "\n";
-					}
-					else
-					{
-						activitydisplay += eachactivity.Name + "\t" + eachactivity.Code + "\t\t" + eachactivity.ECTS + "\t\t" + eachactivity.Teacher.Trigram + "\n";
-					}
-				}
-				Console.WriteLine(activitydisplay);
 			}
-			else
-			{
-				Console.WriteLine("Veuillez entrer l'un des numeros de liste existant");
-			}
+			Console.WriteLine("\nBye");
 		}
 	}
 }
